d7081752f167667986061464888eded6175813ac
Kamarhinanton <Kamarhin_anton@ukr.net>
2022-06-28 14:53:59 +0300

    feat: start project

---
+++ #src/js/script.js
@@ -0,0 +1,32 @@
+window.addEventListener('DOMContentLoaded', function () {
+
+})
+
+// const bg = () => {
+//     document.querySelectorAll(".ibg").forEach(el => {
+//         if (el.querySelector('img')) {
+//             el.style.backgroundImage = 'url(' + el.querySelector('img').getAttribute('src') + ')';
+//             el.querySelector('img').style.display = 'none';
+//         }
+//     });
+// }
+
+// const scrollPage = () => {
+//     let scrollPosition;
+//     const header = document.querySelector('.header');
+//     const scrollChange = 1;
+
+//     const addClass = () => header.classList.add("hide")
+//     const removeClass = () => header.classList.remove("hide")
+
+//     window.addEventListener('scroll', function () {
+//         scrollPosition = window.scrollY;
+//         if (scrollPosition >= scrollChange) { addClass() }
+//         else { removeClass() }
+//     })
+// }
+
+
+
+
+
